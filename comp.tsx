8637a58052ae1cf787dbe3a2f2e42a10c1bb6c49
qicheng.zhang <zqc007@gmail.com>
2023-07-03 15:20:14 +0800

    init(1st): commit

---
+++ comp.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export function SpecialLineWebview() {
+  return <div>SpecialLineWebview</div>;
+}
+
+export function InitPage() {
+  return <div>SpecialLineWebview</div>;
+}
+
+export function TopMarketingImage() {
+  return <div>SpecialLineWebview</div>;
+}
+
+export function BigImageTruck(props: any) {
+  return <div>SpecialLineWebview</div>;
+}
+
+export function AddressNew(props: any) {
+  return <div>SpecialLineWebview</div>;
+}
---
+++ quality-tools.tsx
@@ -0,0 +1,82 @@
+import { ScrollView, View } from '@tarojs/components'
+import React, { useRef, useState } from 'react'
+import { AddressNew, BigImageTruck, InitPage, SpecialLineWebview, TopMarketingImage } from './comp'
+import styles from './quality-tools.scss'
+
+const TITLE_SWITCH_VALUE = {
+  specailLine: 1,
+  qualityTools: 2,
+}
+
+const navHeight = 44
+const tabHeight = 50
+
+function QualityTools() {
+  const scrollViewRef = useRef(null)
+  const [tabIndex, setTabIndex] = useState(1)
+  const [pageLoading, setPageLoading] = useState(true)
+  const [bottomAd, setBottomAd] = useState({
+    resourceId: '',
+  })
+  const [animation, setAnimationNow] = useState(0)
+
+  const handleSelectCargoTypeTrigger = second => {
+    console.log(`🚀 ~ QualityTools ~ second:`, second)
+  }
+
+  const handleScroll = second => {
+    console.log(`🚀 ~ QualityTools ~ second:`, second)
+  }
+
+  const loadInitData = second => {
+    console.log(`🚀 ~ QualityTools ~ second:`, second)
+  }
+
+  const handleTrigger = second => {
+    console.log(`🚀 ~ QualityTools ~ second:`, second)
+  }
+
+  return (
+    <div>
+      {tabIndex === TITLE_SWITCH_VALUE.specailLine ? (
+        <SpecialLineWebview />
+      ) : pageLoading ? (
+        <InitPage />
+      ) : (
+        <ScrollView
+          className={styles.pageCont}
+          scrollWithAnimation
+          // @ts-ignore
+          alwaysBounceVertical={false}
+          style={{ height: `calc(100vh - ${navHeight + tabHeight}px)` }}
+          ref={scrollViewRef}
+        >
+          {/* 顶部营销广告位组件 */}
+          <TopMarketingImage />
+
+          {/* 车型切换swipper组件 */}
+          <BigImageTruck handleSelectCargoTypeTrigger={handleSelectCargoTypeTrigger} animation={animation} />
+
+          {/* 装卸货地址 */}
+          <AddressNew loadInitData={loadInitData} handleTrigger={handleTrigger} setAnimationNow={setAnimationNow} handleScroll={handleScroll} />
+
+          {/* 广告位 */}
+          <View className={styles.bannerBox}>
+            {!!bottomAd?.resourceId && (
+              <View className={styles.marketResource}>
+                {/* <MarketingResource
+          resourceId={bottomAd?.resourceId}
+          env={process.env.NODE_ENV}
+          httpClient={(url, data) => {
+            return http.post(url, data.data, { ignoreError: true })
+          }}></MarketingResource> */}
+              </View>
+            )}
+          </View>
+        </ScrollView>
+      )}
+    </div>
+  )
+}
+
+export default QualityTools
