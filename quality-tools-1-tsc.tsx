35ce33ebb05cc3893d12dfc58c4aef439a8b0957
qicheng.zhang <zqc007@gmail.com>
2023-07-11 09:52:26 +0800

    feat(全局): 增加测试对照组

---
+++ quality-tools-1-tsc.tsx
@@ -0,0 +1,45 @@
+import { ScrollView, View } from '@tarojs/components';
+import React, { useRef, useState } from 'react';
+import { AddressNew, BigImageTruck, InitPage, SpecialLineWebview, TopMarketingImage } from './comp';
+import styles from './quality-tools.scss';
+const TITLE_SWITCH_VALUE = {
+    specailLine: 1,
+    qualityTools: 2,
+};
+const navHeight = 44;
+const tabHeight = 50;
+function QualityTools() {
+    const scrollViewRef = useRef(null);
+    const [tabIndex, setTabIndex] = useState(1);
+    const [pageLoading, setPageLoading] = useState(true);
+    const [bottomAd, setBottomAd] = useState({
+        resourceId: '',
+    });
+    const [animation, setAnimationNow] = useState(0);
+    const handleSelectCargoTypeTrigger = second => {
+        console.log(`🚀 ~ QualityTools ~ second:`, second);
+    };
+    const handleScroll = second => {
+        console.log(`🚀 ~ QualityTools ~ second:`, second);
+    };
+    const loadInitData = second => {
+        console.log(`🚀 ~ QualityTools ~ second:`, second);
+    };
+    const handleTrigger = second => {
+        console.log(`🚀 ~ QualityTools ~ second:`, second);
+    };
+    if (tabIndex === TITLE_SWITCH_VALUE.specailLine) {
+        return React.createElement(SpecialLineWebview, null);
+    }
+    if (pageLoading) {
+        return React.createElement(InitPage, null);
+    }
+    return (React.createElement(ScrollView, { className: styles.pageCont, scrollWithAnimation: true,
+        // @ts-ignore
+        alwaysBounceVertical: false, style: { height: `calc(100vh - ${navHeight + tabHeight}px)` }, ref: scrollViewRef },
+        React.createElement(TopMarketingImage, null),
+        React.createElement(BigImageTruck, { handleSelectCargoTypeTrigger: handleSelectCargoTypeTrigger, animation: animation }),
+        React.createElement(AddressNew, { loadInitData: loadInitData, handleTrigger: handleTrigger, setAnimationNow: setAnimationNow, handleScroll: handleScroll }),
+        React.createElement(View, { className: styles.bannerBox }, !!(bottomAd === null || bottomAd === void 0 ? void 0 : bottomAd.resourceId) && React.createElement(View, { className: styles.marketResource }))));
+}
+export default QualityTools;
